3c7948cf371ac779d2671ffca706009b79d0b8c8
puttarajkoliwad <puttarajkoliwad@gmail.com>
2022-04-29 13:02:59 +0530

    Graphs::DFS::Recursion count islands

---
+++ DSA/DS/Graphs/Edge.java
@@ -0,0 +1,13 @@
+package graphs;
+
+public class Edge {
+    int src;
+    int nbr;
+    int wt;
+
+    public Edge(int src, int nbr, int wt){
+        this.src = src;
+        this.nbr = nbr;
+        this.wt = wt;
+    }
+}
