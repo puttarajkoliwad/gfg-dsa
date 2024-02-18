e9ab16126f12b47343a84c7b8fae4f2ba559e760
puttaraj koliwad <pkoliwad@mdsol.com>
2024-02-18 23:33:51 +0530

    corrects algorithm

--- problem_solving/SummateDifferences.java
+++ problem_solving/SummateDifferences.java
@@ -1,6 +1,8 @@
 // Find the subset of length 'k' from an array of integers such that the sum of absolute differences b/w each pair of elements in the subset is maximum
 
-// Theory: https://excalidraw.com/#json=MPoSzT8n5rJYe__AlAY2r,HNyzIyGy94x8to3t8QqnTQ
+// Theory: https://excalidraw.com/#json=0UcEAfyWXvxJA0kyxdl2v,JMe_XhyH0GVz-sxN1EG_JQ. This theory is wrong, compare [1,6,8,9] to [1,7,8,9], i.e., 26 to 25
+
+import java.util.Arrays;
 
 public class SummateDifferences{
   static int[] selectSubset(int[] arr, int k){
@@ -13,13 +15,22 @@ public class SummateDifferences{
     int max = subArr[1] = arr[arr.length-1];
     int j=2;
     
+    boolean pickStart = true;
+
     while(start <= end && j<k){
-      if(arr[start] - min < max - arr[end]){
+      // pick alternatively
+      
+      // if(arr[start] - min <= max - arr[end]){
+      if(pickStart){
+        // picks from the front of the array
         subArr[j] = arr[start];
         start++;
+        pickStart = false;
       }else{
+        // picks from the back of the array
         subArr[j] = arr[end];
         end--;
+        pickStart = true;
       }
       j++;
     }
@@ -32,9 +43,28 @@ public class SummateDifferences{
     int y=25;
     int z=x+y;
     
-    int[] arr = {1, 5, 2, 3, 9};
+    int[] arr = {1, 4, 9, 8, 6, 7};
+    // int[] arr = {1, 5, 2, 3, 9};
     
-    int[] res = selectSubset(arr, 2);
+    int[] res = selectSubset(arr, 5);
     System.out.println(Arrays.toString(res));
+    int ans = evaluateAbsoluteDifference(res);
+    System.out.println(ans);
+  }
+
+  // static int findIndexMultipliedArraySum(int[] arr){
+  //   int sum = arr[0];
+
+  // }
+
+  static int evaluateAbsoluteDifference(int[] arr){
+    int sum=0;
+    for(int i=0; i<arr.length-1; i++){
+      for(int j=i+1; j<arr.length; j++){
+        sum += Math.abs(arr[i] - arr[j]);
+      }
+    }
+
+    return sum;
   }
 }
